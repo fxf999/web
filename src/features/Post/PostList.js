3ee6bb7aa0bf49f4cf9af4835752022f9091a140
Sebastian Kim <sydneyitguy@gmail.com>
2018-01-13 04:47:00 +0900

    Daily post headings

--- src/features/Post/PostList.js
+++ src/features/Post/PostList.js
@@ -9,6 +9,7 @@ import { selectPosts, selectIsLoading } from './selectors';
 import { getPostsBegin } from './actions/getPosts';
 import { daysAgoToString } from 'utils/date';
 import Post from './components/Post';
+import { formatAmount } from "utils/helpers/steemitHelpers";
 
 class PostList extends Component {
   static propTypes = {
@@ -28,16 +29,20 @@ class PostList extends Component {
 
   render() {
     const { daysAgo, isLoading } = this.props;
+    let dailyTotalReward = 0;
     const posts = (this.props.posts[daysAgo] || []).map((post, index) => {
+      dailyTotalReward += post.payout_value;
       return (
         <Post key={post.id} rank={index + 1} post={post} />
       );
     });
-
     return (
       <Spin size="large" spinning={isLoading}>
-        <h3>{daysAgoToString(daysAgo)}</h3>
         <div className="post-list">
+          <div className="heading">
+            <h3>{daysAgoToString(daysAgo)}</h3>
+            <p><b>{posts.length}</b> products, <b>{formatAmount(dailyTotalReward)}</b> hunter’s rewards were generated.</p>
+          </div>
           {posts}
         </div>
       </Spin>
