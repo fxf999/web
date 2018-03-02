a7c2bb98c0e3884856ffb2d2e9698835820c8bd3
Sebastian Kim <sydneyitguy@gmail.com>
2018-03-02 10:12:51 +0900

    Fix #2 - Open graph on product pages

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import isEmpty from 'lodash/isEmpty';
 import { Helmet } from 'react-helmet';
-
 import { selectAppProps } from './selectors';
 import { getAppConfigBegin } from './actions/getAppConfig';
 import { RoutesLeft, RoutesRight } from 'Routes';
@@ -23,7 +22,23 @@ class App extends Component {
     return (
       <div id="app-container" className="app-container">
         <Helmet>
-          <title>Steemhunt</title>
+          <title>Steemhunt - Dig Products, Earn STEEMs</title>
+          {/* Search Engine */}
+          <meta name="description" content="Daily ranking of effortlessly cool products fueled by STEEM blockchain" />
+          <meta name="image" content="https://steemhunt.com/og-image.jpg" />
+          {/* Schema.org for Google */}
+          <meta itemprop="name" content="Steemhunt - Dig Products, Earn STEEMs" />
+          <meta itemprop="description" content="Daily ranking of effortlessly cool products fueled by STEEM blockchain" />
+          <meta itemprop="image" content="https://steemhunt.com/og-image.jpg" />
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="Steemhunt - Dig Products, Earn STEEMs" />
+          <meta name="twitter:description" content="Daily ranking of effortlessly cool products fueled by STEEM blockchain" />
+          <meta name="twitter:image:src" content="https://steemhunt.com/og-image.jpg" />
+          {/* Open Graph general (Facebook, Pinterest & Google+) */}
+          <meta name="og:title" content="Steemhunt - Dig Products, Earn STEEMs" />
+          <meta name="og:description" content="Daily ranking of effortlessly cool products fueled by STEEM blockchain" />
+          <meta name="og:image" content="https://steemhunt.com/og-image.jpg" />
         </Helmet>
         <div className="split-container">
           <RoutesLeft />
